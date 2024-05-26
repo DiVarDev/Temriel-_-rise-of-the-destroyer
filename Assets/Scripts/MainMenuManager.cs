fb58010da18ef7ce08b61b0894b4f0870e3adece
DiVarDev <118238723+DiVarDev@users.noreply.github.com>
2024-05-26 14:06:08 -0500

    Added scenes and a pizza texture

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Functions
+    public void QuitGame()
+    {
+        // Closing application
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}

4624cda057eb502e16a2c80a6b68258ba13d6710
DiVarDev <118238723+DiVarDev@users.noreply.github.com>
2024-05-31 09:48:52 -0500

    Added countdown to level 1 and the bad ending scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,12 +9,26 @@ public class GameManager : MonoBehaviour
     private SceneLoader sceneLoader;
     [Header("Scenes indexes")]
     public int goodEnding = 2;
+    public int badEnding = 3;
     public GameObject boss;
+    [Header("Timer")]
+    public TimeManager timeManager;
+    public TMP_Text timerText;
+    public float time;
+    public float maxTime = 120;
 
     // Start is called before the first frame update
     void Start()
     {
         sceneLoader = GetComponent<SceneLoader>();
+
+        timeManager = GetComponent<TimeManager>();
+
+        time = maxTime;
+
+        UpdateTimerText(time, maxTime);
+
+        StartCoroutine(CountdownTimer());
     }
 
     // Update is called once per frame
@@ -23,11 +38,38 @@ public class GameManager : MonoBehaviour
         {
             LoadGoodEnding();
         }
+
+        if (time <= 0.0f)
+        {
+            LoadBadEnding();
+        }
+    }
+
+    // Corountines
+    IEnumerator CountdownTimer()
+    {
+        while (time >= 0.0f)
+        {
+            UpdateTimerText(time, maxTime);
+            yield return new WaitForSeconds(1.0f);
+            time--;
+        }
     }
 
     // Functions
+    public void UpdateTimerText(float time, float maxTime)
+    {
+        timeManager.CaculateTimer(time, maxTime);
+        timerText.text = timeManager.ReturnStringTimeElapsed();
+    }
+
     public void LoadGoodEnding()
     {
         sceneLoader.LoadSceneAsyncSingleByIndex(goodEnding);
+    }
+
+    public void LoadBadEnding()
+    {
+        sceneLoader.LoadSceneAsyncSingleByIndex(badEnding);
     }
 }
